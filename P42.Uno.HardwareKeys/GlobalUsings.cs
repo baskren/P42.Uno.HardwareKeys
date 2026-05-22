38afa96e636baed3fffa9f9c4cd4fa37b85278dd
Ben Askren <ben@buildcalc.com>
2026-05-22 12:31:05 -0400

    REFACTOR: Able to build

--- P42.Uno.HardwareKeys/GlobalUsings.cs
+++ P42.Uno.HardwareKeys/GlobalUsings.cs
@@ -1,7 +1,4 @@
-﻿global using Microsoft.UI.Xaml.Controls;
+global using Microsoft.UI.Xaml.Controls;
 global using Microsoft.UI.Xaml.Controls.Primitives;
 global using Microsoft.UI.Xaml.Input;
 global using Microsoft.UI.Xaml.Shapes;
-global using P42.Utils;
-global using P42.Utils.Uno;
-global using P42.Uno.WinUI.Markup;
